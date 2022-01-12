321c12540243cd91980b34986e2538830ceeea9f
127.0.0.1 localhost <qwerty97001@outlook.es>
2022-01-12 08:50:49 +0100

    UT3 Parte 2

--- app/src/main/java/com/politecnico/dam/DashboardActivity.java
+++ app/src/main/java/com/politecnico/dam/DashboardActivity.java
@@ -2,7 +2,10 @@ package com.politecnico.dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DashboardActivity extends AppCompatActivity {
 
@@ -11,4 +14,10 @@ public class DashboardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
     }
+    public void startMapWindow(View view) {
+        Intent intent = new Intent(DashboardActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+
 }

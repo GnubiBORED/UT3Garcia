f4b473b65dc09de194a6babb7ee915899da7e10b
127.0.0.1 localhost <qwerty97001@outlook.es>
2022-01-14 07:57:00 +0100

    Json prueba

--- app/src/main/java/com/politecnico/dam/SaludActivity.java
+++ app/src/main/java/com/politecnico/dam/SaludActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
